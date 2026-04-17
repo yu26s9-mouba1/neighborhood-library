56d01853c1b99b85cc4c875aa47c196ce0f0334d
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-17 14:29:56 -0400

    Created constructors, setters and getters for the bokks library class

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -9,13 +9,57 @@ public class Book {
     private boolean isCheckedOut;
     private String checkedOutTo;
 
-
-    //Creating methods to identify either a book is checkedin or out
-    public Book(int bookID, String bookIsbn, String bookTittle, boolean isCheckedOut, String checkedOutTo ){
-
-        
-
+    //Constructors
+    public Book(int bookID, String bookIsbn, String bookTittle, boolean isCheckedOut, String checkedOutTo) {
+        this.bookID = bookID;
+        this.bookIsbn = bookIsbn;
+        this.bookTittle = bookTittle;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
     }
+
+
+    //Created setters and getters 
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookIsbn() {
+        return bookIsbn;
+    }
+
+    public void setBookIsbn(String bookIsbn) {
+        this.bookIsbn = bookIsbn;
+    }
+
+    public String getBookTittle() {
+        return bookTittle;
+    }
+
+    public void setBookTittle(String bookTittle) {
+        this.bookTittle = bookTittle;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+}
 
 
 
---
+++ src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class NeighborhoodLibraryApp {
+    Scanner scanner = new Scanner(System.in);
+
+
+}
