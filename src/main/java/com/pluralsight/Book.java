4eb59ca048341def67f143d38c9b5f8dd8664228
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-17 13:10:28 -0400

    Created book properties

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -2,4 +2,24 @@ package com.pluralsight;
 
 public class Book {
 
+    //Creating the book properties
+    private int bookID;
+    private String bookIsbn;
+    private String bookTittle;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+
+    //Creating methods to identify either a book is checkedin or out
+    public Book(int bookID, String bookIsbn, String bookTittle, boolean isCheckedOut, String checkedOutTo ){
+
+        
+
+    }
+
+
+
+
+
+
 }
